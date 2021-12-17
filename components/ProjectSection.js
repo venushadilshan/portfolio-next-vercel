9a8b72e0c853a8b3a3466f432158bfacd7801416
Venusha Dilshan Dushmantha <venusha125@gmail.com>
2021-12-17 14:04:44 +0530

    Add Viewer

--- components/ProjectSection.js
+++ components/ProjectSection.js
@@ -31,9 +31,7 @@ const ProjectSection = () => {
                 </div>
                 <div className="center-container">
                     <Link href={{
-                        pathname: '/Viewer',
-                        query: { url: 'https://github.com/venushadilshan' },
-                    }}><div className="bg-violet-500 self-center text-slate-200 px-4 py-5 w-44 rounded  hover:bg-violet-800 duration-200 ">See more on GitHub</div></Link>
+                        pathname: 'https://github.com/venushadilshan' }}><div className="bg-violet-500 self-center text-slate-200 px-4 py-5 w-44 rounded  hover:bg-violet-800 duration-200 ">See more on GitHub</div></Link>
                 </div>
             </div>
 
